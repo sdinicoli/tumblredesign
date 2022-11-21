6a0c5b25f3efaf79a03dd456ceccd4d4d63c1a1a
Sonia Di Nicoli <soniadinicoli@gmail.com>
2022-11-21 10:25:16 +0100

    Initial Commit

---
+++ tumblredesign/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tumblredesign
+//
+//  Created by Sonia Di Nicoli on 21/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tumblredesign/tumblredesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  tumblredesignApp.swift
+//  tumblredesign
+//
+//  Created by Sonia Di Nicoli on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct tumblredesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

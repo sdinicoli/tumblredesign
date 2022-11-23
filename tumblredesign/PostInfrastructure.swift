04ec290beafb4520669765550cb637fcfdd214c7
Sonia Di Nicoli <soniadinicoli@gmail.com>
2022-11-23 23:14:05 +0100

    bjbbbjbh

--- tumblredesign/PostInfrastructure.swift
+++ tumblredesign/PostInfrastructure.swift
@@ -14,7 +14,6 @@ struct PostInfrastructure: View {
     var body: some View {
         
         ZStack{
-            
             RoundedRectangle(cornerRadius: 11)
                 .foregroundColor(Color("light-gray"))
                 .padding()
